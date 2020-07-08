8f0405fc14cf90193920623de3369fc36b29a8e4
Inigo-santiu <67637261+Inigo-santiu@users.noreply.github.com>
2020-07-08 10:04:30 +0200

    Update exo_13.rb

--- exo_13.rb
+++ exo_13.rb
@@ -1,4 +1,5 @@
 puts "What year were you born?"
 birthyear = gets.chomp
 puts "You lived through... :"
-(birthyear.to_i..2019).each { |i| puts {i + 1}
+(birthyear.to_i..2019).each { |i| puts "#{i + 1}"
+  }
